712a8e0261b18ce0b299442fffd01693d75e339b
Aditya Nath <adityakaushal123@gmail.com>
2026-09-14 02:49:51 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# Student Performance 
+This project analyzes student performance data to identify factors that affect academic success.
+It includes data preprocessing, visualization and predictive modelingusing Python.
+
+## Features
+  - Data cleaning and transformation
+  - Exploratory data analysis (EDA)
+  - Machine Learning model for performance prediction
+  - Visualization of key insights
+
+## Technologies Used
+- Python
+- Pandas
+- Numpy
+- Matplotlib
+- Scikit-learn
